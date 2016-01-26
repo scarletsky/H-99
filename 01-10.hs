833a1c36f0d54a9062f03463d28fb155281e9b17
scarletsky <scarletsky1025@gmail.com>
2016-01-26 11:07:59 +0800

    Format the code

--- 01-10.hs
+++ 01-10.hs
@@ -1,30 +1,30 @@
 -- (*) Find the last element of a list.
 
 myLast1 :: [a] -> a
-myLast1 [] = error "Empty list"
+myLast1 []     = error "Empty list"
 myLast1 (x:xs) = if (null xs) then x else myLast1 xs  
 
 myLast2 :: [a] -> a
-myLast2 [] = error "Empty list"
-myLast2 [x] = x
+myLast2 []     = error "Empty list"
+myLast2 [x]    = x
 myLast2 (_:xs) = myLast2 xs
 
 myLast3 :: [a] -> a
-myLast3 [] = error "Empty list"
+myLast3 []     = error "Empty list"
 myLast3 (x:[]) = x
 myList3 (_:xs) = myLast3 xs
 
 
 -- (*) Find the last but one element of a list.
 myButLast :: [a] -> a
-myButLast [] = error "Empty List"
+myButLast []       = error "Empty List"
 myButLast (x:_:[]) = x 
-myButLast (_:xs) = myButLast xs
+myButLast (_:xs)   = myButLast xs
 
 
 -- (*) Find the K'th element of a list. The first element in the list is number 1.
-elementAt [] _    = error "Empty list"
-elementAt (x:_) 1 = x
+elementAt [] _       = error "Empty list"
+elementAt (x:_) 1    = x
 elementAt all@(_:xs) n
     | n < 1          = error "Index should greater than 0"
     | n > length all = error "Index should less than list's length"
@@ -33,7 +33,7 @@ elementAt all@(_:xs) n
 
 -- (*) Find the number of elements of a list.
 myLength :: (Num b) => [a] -> b
-myLength [] = 0
+myLength []     = 0
 myLength (_:xs) = 1 + myLength xs
 
 -- (*) Reverse a list.
