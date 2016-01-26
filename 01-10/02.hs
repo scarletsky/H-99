3dcebd4ee93c7285632056e82a2ddac55da991eb
scarletsky <scarletsky1025@gmail.com>
2016-01-26 11:02:33 +0800

    Merge files

--- 01-10/02.hs
+++
@@ -1,5 +0,0 @@
-myButLast :: [a] -> a
-myButLast [] = error "Empty List"
-myButLast (x:_:[]) = x 
-myButLast (_:xs) = myButLast xs
-
--- 01-10/03.hs
+++
@@ -1,7 +0,0 @@
-elementAt [] _    = error "Empty list"
-elementAt (x:_) 1 = x
-elementAt all@(_:xs) n
-    | n < 1          = error "Index should greater than 0"
-    | n > length all = error "Index should less than list's length"
-    | otherwise      = elementAt xs (n-1)
-
