bad5556c72094a89c1f9ebfa8a782791b76f0358
scarletsky <scarletsky1025@gmail.com>
2016-01-28 21:48:29 +0800

    Add 07 solution

--- 01-10.hs
+++ 01-10.hs
@@ -48,3 +48,10 @@ isPalindrome :: (Eq a) => [a] -> Bool
 isPalindrome xs = xs == myReverse xs 
 
 
+-- 7 (**) Flatten a nested list structure.
+data NestedList a     = Elem a | List [NestedList a] deriving (Show)
+flatten :: NestedList a -> [a]
+flatten (Elem x)      = [x]
+flatten (List [])     = []
+flatten (List (x:xs)) = flatten x ++ flatten (List xs)
+
