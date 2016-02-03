bf90e8b358f9014d5edf5103e8ae1080fdf5c318
scarletsky <scarletsky1025@gmail.com>
2016-02-03 17:32:01 +0800

    Add 14 solution

--- 11-20.hs
+++ 11-20.hs
@@ -7,8 +7,18 @@ data Code = Single Char
 
 encodeModified = map (\(all@(y:ys)) -> if (length all == 1) then Single y else Multiple (length all) y) . group
 
+
 -- 12 (**) Decode a run-length encoded list.
 decodeModified [] = []
 decodeModified ((Single x):xs) = x : decodeModified xs
 decodeModified ((Multiple n x):xs) = replicate n x ++ decodeModified xs
+
+
+-- 14 (*) Duplicate the elements of a list.
+dupli :: [a] -> [a]
+dupli []     = []
+dupli (x:xs) = x : x : dupli xs
+
+dupli' :: [a] -> [a]
+dupli' = concatMap (\x -> x : [x])
 
