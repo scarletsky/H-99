82f1012d790cba43531c532b95bf66687d1e7e62
scarletsky <scarletsky1025@gmail.com>
2016-02-04 14:06:16 +0800

    Add more solution in 11-20

--- 11-20.hs
+++ 11-20.hs
@@ -5,13 +5,25 @@ data Code = Single Char
           | Multiple Int Char
           deriving (Show)
 
-encodeModified = map (\(all@(y:ys)) -> if (length all == 1) then Single y else Multiple (length all) y) . group
+encodeModified = map f . group
+    where f (all@(y:ys))
+            | length all == 1 = Single y
+            | otherwise       = Multiple (length all) y
 
 
 -- 12 (**) Decode a run-length encoded list.
 decodeModified [] = []
 decodeModified ((Single x):xs) = x : decodeModified xs
 decodeModified ((Multiple n x):xs) = replicate n x ++ decodeModified xs
+
+
+-- 13 (**) Run-length encoding of a list (direct solution).
+encodeDirect [] = []
+encodeDirect (x:xs)
+    | count == 1 = Single x : encodeDirect rest
+    | otherwise  = Multiple count x : encodeDirect rest
+    where (matched, rest) = span (== x) xs
+          count = 1 + length matched
 
 
 -- 14 (*) Duplicate the elements of a list.
@@ -45,6 +57,9 @@ dropEvery' xs n
                    then h 
                    else init h 
 
+dropEvery'' [] _ = []
+dropEvery'' xs n = (init . take n) xs ++ dropEvery'' (drop n xs) n
+
 
 -- 17 (*) Split a list into two parts; the length of the first part is given.
 split :: [a] -> Int -> ([a], [a])
@@ -53,6 +68,9 @@ split xs     0   = ([], xs)
 split (x:xs) n   = (x : h, t)
     where (h, t) = split xs (n - 1)
 
+split' xs n = (take n xs, drop n xs)
+
+split'' = splitAt
 
 -- 18 (**) Extract a slice from a list.
 slice :: [a] -> Int -> Int -> [a]
@@ -60,6 +78,15 @@ slice [] _ _        = []
 slice xs i k        = map snd $ filter f xs'
     where xs'       = zip [1..] xs
           f (i', _) = (i' >= i && i' <= k)
+
+slice' [] _ _ = []
+slice' (all@(x:xs)) i k =  g all i k 1
+    where g (y:ys) i k count
+            | count < i = g ys i k (count + 1)
+            | count > k = []
+            | otherwise = y : g ys i k (count + 1)
+
+slice'' xs i k = drop (i-1) $ take k xs
 
 
 -- 19 (**) Rotate a list N places to the left.
@@ -69,6 +96,12 @@ rotate xs n  = snd ys ++ fst ys
     where ys = split xs n'
           n' | n >= 0 = n
              | n < 0  = length xs - abs n
+
+rotate' [] _     = []
+rotate' xs 0     = xs
+rotate' (x:xs) n
+    | n < 0      = rotate' xs (length xs - abs n) ++ [x]
+    | otherwise  = rotate' xs (n - 1) ++ [x]
 
 
 -- 20 (*) Remove the K'th element from a list.
