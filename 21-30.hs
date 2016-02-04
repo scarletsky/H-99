74550257b72e479d00ef58fd951a34740794e051
scarletsky <scarletsky1025@gmail.com>
2016-02-04 14:53:42 +0800

    Add 22 solution

--- 21-30.hs
+++ 21-30.hs
@@ -7,3 +7,12 @@ insertAt' x ys i = map snd as ++ x : map snd bs
           f (j, _) = j < i
           (as, bs) = span f ys'
 
+
+-- 22 Create a list containing all integers within a given range.
+range :: Int -> Int -> [Int]
+range i j
+    | i == j = [j]
+    | otherwise = i : range (i+1) j
+
+range' i j = [i..j]
+
