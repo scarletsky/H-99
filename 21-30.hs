961044e5e6e731b8ab0af70f3feec128f01ab799
scarletsky <scarletsky1025@gmail.com>
2016-02-11 17:20:14 +0800

    Add 27 solution

--- 21-30.hs
+++ 21-30.hs
@@ -51,3 +51,18 @@ combinations _ [] = []
 combinations 1 xs = map (\x -> [x]) xs
 combinations n (x:xs) = combinations n xs ++ (map (x:) $ combinations (n-1) xs)
 
+
+-- 27 Group the elements of a set into disjoint subsets.
+
+combination :: Int -> [a] -> [([a], [a])]
+combination 0 xs     = [([], xs)]
+combination n []     = []
+combination n (x:xs) = ts ++ ds
+    where
+        ts = [ (x:ys, zs) | (ys, zs) <- combination (n-1) xs ]
+        ds = [ (ys, x:zs) | (ys, zs) <- combination n     xs ]
+
+group' [] _ = [[]]
+group' (n:ns) xs =
+    [ g:gs | (g, rs) <- combination n xs
+           , gs      <- group' ns rs ]
