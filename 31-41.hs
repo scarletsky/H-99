45bc20e21b565d02f8f082fdca6b1c36a7f9b9e8
scarletsky <scarletsky1025@gmail.com>
2016-03-04 23:26:21 +0800

    Add 32 solution

--- 31-41.hs
+++ 31-41.hs
@@ -3,3 +3,7 @@ isPrime 2 = True
 isPrime 3 = True
 isPrime n = 0 `notElem` (map f [2..(n-1)])
     where f x = n `mod` x
+
+
+-- 32 (**) Determine the greatest common divisor of two positive integer numbers.
+myGCD x y = if (x `mod` y == 0) then y else myGCD y (x `mod` y)
