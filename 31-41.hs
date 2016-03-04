947ab084957d41497198827c835188b30cf7057f
scarletsky <scarletsky1025@gmail.com>
2016-03-04 23:40:32 +0800

    Add 33 solution

--- 31-41.hs
+++ 31-41.hs
@@ -9,4 +9,9 @@ isPrime n = 0 `notElem` (map f [2..(n-1)])
 myGCD x y = if (x `mod` y == 0) then y else myGCD y (x `mod` y)
 
 myGCD' x 0 = abs x
-myGCD' x y = myGCD y (x `mod` y)
+myGCD' x y = myGCD' y (x `mod` y)
+
+-- 33 (*) Determine whether two positive integer numbers are coprime. Two numbers are coprime if their greatest common divisor equals 1.
+coprime x y = if (myGCD' x y == 1) then True else False
+
+coprime' x y = myGCD' x y == 1
